31ddbb3a5e565d326fc27f685f204c0166572a0c
Wondmagegnehu Wondimu <wondmagegne@yahoo.com>
2022-02-16 17:46:33 -0500

    New property added

--- TheSimpleDeviceBase/DeviceOne.cs
+++ TheSimpleDeviceBase/DeviceOne.cs
@@ -7,5 +7,7 @@ namespace TheSimpleDeviceBase
         public string Name { get; set; }
 
         public string DeviceIDmain { get; set; }
+
+        public string MoreNameProp { get; set; }
     }
 }
